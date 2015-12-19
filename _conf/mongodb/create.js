777718f51d4db8f8e3d559a8b50b0a2755a18e82
Ira Klotzko <ik@iratesolutions.com>
2015-12-19 12:52:29 -0500

    added a create file

---
+++ _conf/mongodb/create.js
@@ -0,0 +1,10 @@
+db.runCommand('use priorityze');
+
+db.createUser({
+    "user": "padmin",
+    "pwd": "p123",
+    "roles": [
+        { role: "userAdmin", db: "priorityze" },
+        "readWrite"
+    ]
+},  { w: "majority", wtimeout: 5000});
