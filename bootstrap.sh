9537107a6493ad93f07744eb5bfee2639370a139
Jason <jasonpau@gmail.com>
2016-01-05 22:26:00 -0500

    Finished provisioning for vagrant machine

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+#Get rid of the old node
 sudo apt-get purge nodejs npm
 apt-get update
 sudo apt-get install -y python-software-properties python g++ make
@@ -9,3 +10,24 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y npm
 sudo apt-get install -y git
 sudo npm install -g bower
+sudo npm install -g rode
+#mongo db
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-org
+#not sure this should be part of provisioning
+mongo priorityze /vagrant/_conf/mongodb/create.js
+#redis
+sudo apt-get install build-essential
+sudo apt-get install tcl8.5
+wget http://download.redis.io/releases/redis-stable.tar.gz
+tar xzf redis-stable.tar.gz
+cd redis-stable
+make
+sudo make install
+#nodemon
+npm install -g nodemon
+
+
+
