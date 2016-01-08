4765d9e4905ea3ec6f97ae3e47215bda96ae8e81
Jason <jasonpau@gmail.com>
2016-01-07 23:19:06 -0500

    provision mongo problem

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ sudo npm install -g rode
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
-sudo apt-get install mongodb-org
+sudo apt-get install -y mongodb-org
 #not sure this should be part of provisioning
 mongo priorityze /vagrant/_conf/mongodb/create.js
 #redis
